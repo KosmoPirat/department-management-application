71cb91ff052d7a2736985a765f7eb885279bd126
KosmoPirat <kosmopirat@gmail.com>
2019-11-15 17:47:10 +0300

    Update Menu.js

--- src/app/components/Menu.js
+++ src/app/components/Menu.js
@@ -6,7 +6,6 @@ export default class Menu extends Component {
         super(props);
     }
     render() {
-        console.log(this.props.isAuth);
         return (
                 <nav className="navbar navbar-expand-lg navbar-light bg-light">
                     <Link to="/" className="navbar-brand">
